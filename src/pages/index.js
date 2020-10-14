7792b4c3c508d9eb01218531b25d06acb3cd5f27
Jolene Wai <jolene@overdose.digital>
2020-10-14 16:26:40 +0800

    Replaced H1 with Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div style={{ color: 'purple' }}>
       <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
+      <Header headerText="Hello Gatsby" />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>)
